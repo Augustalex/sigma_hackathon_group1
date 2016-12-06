6a1c3bcdf287ffd4f42385d6f65eb8734ff3a0fe
Martin Persson <xarragon@gmail.com>
2016-12-06 19:58:37 +0100

    Translator: Clean up imports

--- Translator/src/translator/BingTrans.java
+++ Translator/src/translator/BingTrans.java
@@ -1,14 +1,13 @@
-import java.net.URLConnection;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.*;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
